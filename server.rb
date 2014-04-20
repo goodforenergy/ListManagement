4f6203881cbe0770619429abd9b026dfc363e95b
Helen Durrant <hh.durrant@gmail.com>
2014-04-20 17:31:41 +1200

    Setting things up for deployment to Heroku.

--- server.rb
+++ server.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+set :public_folder, File.dirname(__FILE__) + '/public'
+
 get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
+  send_file File.expand_path('index.html', settings.public_folder)
 end
